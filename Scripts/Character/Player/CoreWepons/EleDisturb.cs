7994d66f65597c0152a82c1314287251999c8a92
Dfeather-Toufu <941380131@qq.com>
2025-04-26 17:46:59 +0800

    一些武器代码改动

--- Scripts/Character/Player/CoreWepons/EleDisturb.cs
+++ Scripts/Character/Player/CoreWepons/EleDisturb.cs
@@ -5,12 +5,41 @@ public class EleDisturb : CoreWeponBase
 {
     public float EffectDistance;
     public float Damage;
+    [HideInInspector]public CapsuleCollider2D CheckCycle;
     [HideInInspector] public int level;
     [HideInInspector] public Animator animator;
+    public enum States{
+        Normal,Fire
+    }
+    private Dictionary<States, ICoreWpeonsStateMacine> StateDic = new Dictionary<States, ICoreWpeonsStateMacine>();
     public override void Awake_Func()
     {
         base.Awake_Func();
+        CheckCycle =GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
+        DistanceExchange(1);
+        StateDic.Add(States.Normal,new EleNormal(this));
+        StateDic.Add(States.Fire, new EleEnable(this));
+        nowState=StateDic[States.Normal];
     }
-
+    private void FixedUpdate()
+    {
+        nowState.FixedUpdate();
+    }
+    private void Update()
+    {
+        nowState.Update();
+    }
+    //×´Ì¬×ª»»º¯Êý
+    public void StatesTrans_Func(States aim)
+    {
+        nowState.OnDisable();
+        nowState = StateDic[aim];
+        nowState.OnEnable();
+    }
+    //¹¥»÷¾àÀëÐÞ¸Ä
+    public void DistanceExchange(float Xtime)
+    {
+        CheckCycle.size = EffectDistance * Xtime * Vector2.one;
+    }
 }
---
+++ Scripts/Character/Player/CoreWepons/ICoreWpeonsStateMacine.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ÎäÆ÷½Ó¿Ú
+public interface ICoreWpeonsStateMacine 
+{
+    public void OnEnable();
+    public void Update();
+    public void FixedUpdate();
+    public void OnDisable();
+}
+//µç´ÅµÄ¹¥»÷/Æô¶¯×´Ì¬
+public class EleEnable:ICoreWpeonsStateMacine
+{
+    EleDisturb Ele;
+    public EleEnable(EleDisturb eleDisturb)
+    {
+        Ele = eleDisturb;
+    }
+    public void OnEnable()
+    {
+        Ele.CheckCycle.enabled = true;
+    }
+    public void Update()
+    {
+        if(Input.GetMouseButtonUp(ControllerPerfence.A1mousenumber)||Input.GetKeyUp(ControllerPerfence.A1))
+        {
+            Ele.StatesTrans_Func(EleDisturb.States.Normal);
+        }
+    }
+    public void FixedUpdate()
+    {
+
+    }
+    public void OnDisable()
+    {
+        Ele.CheckCycle.enabled =false;
+    }
+}
+//µç´ÅµÄÏÐÖÃ×´Ì¬
+public class EleNormal : ICoreWpeonsStateMacine
+{
+    EleDisturb Ele;
+    public EleNormal(EleDisturb eleDisturb)
+    {
+        Ele=eleDisturb;
+    }
+    public void FixedUpdate()
+    {
+        Ele.Follow_Func();
+    }
+
+    public void OnDisable()
+    {
+        
+    }
+
+    public void OnEnable()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
